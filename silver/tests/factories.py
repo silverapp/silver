864ca84558e9984399dd02fba611afdafd2d5015
Emanuel Danci <danci.emanuel@gmail.com>
2014-11-14 20:40:30 +0200

    Update the factory for Provider

--- silver/tests/factories.py
+++ silver/tests/factories.py
@@ -6,3 +6,10 @@ from silver.models import Provider
 class ProviderFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Provider
+
+    name = factory.Sequence(lambda n: 'Provider{cnt}'.format(cnt=n))
+    company = factory.Sequence(lambda n: 'Company{cnt}'.format(cnt=n))
+    address_1 = factory.Sequence(lambda n: 'Address_1{cnt}'.format(cnt=n))
+    country = 'RO'
+    city = factory.Sequence(lambda n: 'City{cnt}'.format(cnt=n))
+    zip_code = factory.Sequence(lambda n: '{cnt}'.format(cnt=n))
