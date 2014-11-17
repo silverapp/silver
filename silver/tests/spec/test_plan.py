a35625d5e74d8e2899bce8ede83e3258a3cd89b1
bogdanpetrea <bogdan@presslabs.com>
2014-11-17 13:20:47 +0200

    Added a couple of Plan tests.

--- silver/tests/spec/test_plan.py
+++ silver/tests/spec/test_plan.py
@@ -2,36 +2,62 @@
 # vim: ft=python:sw=4:ts=4:sts=4:et:
 import json
 
-from silver.models import Plan
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient, APITestCase
 
-from django.test.client import Client
-from django.test import TestCase
 
+class TestPlanEndpoint(APITestCase):
+    fixtures = ['test_data.json']
 
-class PlansSpecificationTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
+        self.client = APIClient()
+
+        self.user_staff = User.objects.create_user(
+            'staff', email='a@b.c', password='abc')
+        self.user_staff.is_staff = True
+        self.user_staff.save()
+
+        self.token_staff = Token.objects.create(user=self.user_staff)
+        self.token_staff.save()
 
     def test_create_plan(self):
-        assert True
-        # response = self.client.put('/api/plans', json.dumps({
-            # 'name': 'Hydrogen',
-            # 'interval': 'month',
-            # 'interval_count': 1,
-            # 'amount': 150,
-            # 'currency': 'USD',
-            # 'trial_period_days': 15,
-            # 'metered_features': [
-                # {
-                    # 'name': '100k PageViews',
-                    # 'price_per_unit': 10,
-                    # 'included_units': 5
-                # }
-            # ],
-            # 'due_days': 10,
-            # 'generate_after': 86400
-        # }), content_type='application/json')
+        self.client.force_authenticate(self.user_staff, self.token_staff)
 
-        # plan = Plan.objects.filter(name='Hydrogen')
-        # self.assertEqual(plan.count(), 1)
-        # self.assertEqual(response.status_code, 201)
+        response = self.client.put('/plans/', json.dumps({
+            "name": "Hydrogen",
+            "interval": "month",
+            "interval_count": 1,
+            "amount": 149.99,
+            "currency": "USD",
+            "trial_period_days": 15,
+            "due_days": 10,
+            "generate_after": 86400,
+            "enabled": True,
+            "private": False,
+            "product_code": "1234",
+            'metered_features': [
+                {
+                    'name': '100k PageViews',
+                    'price_per_unit': 10,
+                    'included_units': 5
+                }
+            ],
+        }), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_plan_list(self):
+        self.client.force_authenticate(self.user_staff, self.token_staff)
+
+        response = self.client.get('/plans/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(response.data, [])
+
+    def test_get_plan_detail(self):
+        self.client.force_authenticate(self.user_staff, self.token_staff)
+        response = self.client.get('/plans/1/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(response.data, [])
