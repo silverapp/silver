af49c53d3c2da47e9a45173d5b804471fdcc0a74
bogdanpetrea <bogdan@presslabs.com>
2014-11-17 14:58:55 +0200

    Added PlanFactory and MeteredFeatureFactory.

--- silver/tests/factories.py
+++ silver/tests/factories.py
@@ -2,7 +2,16 @@ import factory
 
 from django.contrib.auth import get_user_model
 
-from silver.models import Provider
+from silver.models import Provider, Plan, INTERVALS, MeteredFeature
+
+
+class MeteredFeatureFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MeteredFeature
+
+    name = factory.Sequence(lambda n: 'Name{cnt}'.format(cnt=n))
+    price_per_unit = factory.Sequence(lambda n: n)
+    included_units = factory.Sequence(lambda n: n)
 
 
 class ProviderFactory(factory.django.DjangoModelFactory):
@@ -17,6 +26,34 @@ class ProviderFactory(factory.django.DjangoModelFactory):
     zip_code = factory.Sequence(lambda n: '{cnt}'.format(cnt=n))
 
 
+class PlanFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Plan
+
+    name = factory.Sequence(lambda n: 'Name{cnt}'.format(cnt=n))
+    interval = factory.Sequence(lambda n: INTERVALS[n % 4])
+    interval_count = factory.Sequence(lambda n: n)
+    amount = factory.Sequence(lambda n: n)
+    currency = 'USD'
+    trial_period_days = factory.Sequence(lambda n: n)
+    due_days = factory.Sequence(lambda n: n)
+    generate_after = factory.Sequence(lambda n: n)
+    enabled = factory.Sequence(lambda n: n % 2 != 0)
+    private = factory.Sequence(lambda n: n % 2 != 0)
+    product_code = factory.Sequence(lambda n: '{cnt}'.format(cnt=n))
+    provider = factory.SubFactory(ProviderFactory)
+
+    @factory.post_generation
+    def metered_features(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for metered_feature in extracted:
+                self.metered_features.add(metered_feature)
+
+
 class AdminUserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_user_model()
@@ -27,4 +64,3 @@ class AdminUserFactory(factory.django.DjangoModelFactory):
     is_active = True
     is_superuser = True
     is_staff = True
-
