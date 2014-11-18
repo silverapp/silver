47e95261b3c0b2a5a271bc1991a15ae4e6581f1f
bogdanpetrea <bogdan@presslabs.com>
2014-11-18 11:35:34 +0200

    Added Metered Feature API tests.

---
+++ silver/tests/spec/test_metered_feature.py
@@ -0,0 +1,107 @@
+import json
+from django.core import serializers
+import pytest
+
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from silver.tests.factories import AdminUserFactory, MeteredFeatureFactory
+
+
+class TestMeteredFeatureEndpoint(APITestCase):
+    def setUp(self):
+        admin_user = AdminUserFactory.create()
+        self.client.force_authenticate(user=admin_user)
+
+    def test_create_post_metered_feature(self):
+        url = reverse('silver_api:metered-feature-list')
+
+        response = self.client.post(url, json.dumps({
+            "name": "Page Views",
+            "price_per_unit": 0.05,
+            "included_units": 0
+        }), content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual({'name': u'Page Views',
+                          'price_per_unit': 0.05,
+                          'included_units': 0.0,
+                          'url': 'http://testserver/metered-features/1/'},
+                         response.data)
+
+    def test_create_post_metered_feature_without_required_field(self):
+        url = reverse('silver_api:metered-feature-list')
+
+        complete_data = {
+            "name": "Page Views",
+            "price_per_unit": 0.05,
+            "included_units": 0
+        }
+        required_fields = ['name', 'price_per_unit', 'included_units']
+        for field in required_fields:
+            temp_data = complete_data.copy()
+            try:
+                temp_data.pop(field)
+            except KeyError:
+                pytest.xfail('Metered Feature required field %s not provided in'
+                             'the complete test data.' % field)
+
+            response = self.client.post(url, json.dumps(temp_data),
+                                        content_type='application/json')
+
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(response.data,
+                             {field: [u'This field is required.']})
+
+    def test_create_post_metered_feature_bulk(self):
+        mfs = MeteredFeatureFactory.create_batch(7)
+
+        raw_mfs = json.loads(serializers.serialize('json', mfs))
+
+        serialized_mfs = []
+        for item in raw_mfs:
+            serialized_mfs.append(item['fields'])
+
+        url = reverse('silver_api:metered-feature-list')
+
+        response = self.client.post(url, data=json.dumps(serialized_mfs),
+                                    content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 7)
+
+    def test_delete_metered_feature(self):
+        MeteredFeatureFactory.create()
+
+        url = reverse('silver_api:metered-feature-detail', kwargs={'pk': 1})
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.data,
+                         {u'detail': u"Method 'DELETE' not allowed."})
+
+    def test_edit_put_metered_feature(self):
+        MeteredFeatureFactory.create()
+
+        url = reverse('silver_api:metered-feature-detail', kwargs={'pk': 1})
+
+        response = self.client.put(url)
+
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.data,
+                         {u'detail': u"Method 'PUT' not allowed."})
+
+    def test_edit_patch_metered_feature(self):
+        MeteredFeatureFactory.create()
+
+        url = reverse('silver_api:metered-feature-detail', kwargs={'pk': 1})
+
+        response = self.client.patch(url)
+
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(response.data,
+                         {u'detail': u"Method 'PATCH' not allowed."})
