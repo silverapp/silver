cd4ca75338b96262bc023834f16e622310a862b6
Emanuel Danci <danci.emanuel@gmail.com>
2014-11-24 15:12:37 +0200

    Move the settings to the root of the project

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'silver.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_dev')
 
     from django.core.management import execute_from_command_line
 
