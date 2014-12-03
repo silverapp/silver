17ee4cd175dfec5966cf8613f9a7a99b2892df74
Emanuel Danci <danci.emanuel@gmail.com>
2014-12-03 11:06:48 +0200

    Update the historical data for customer/provider if an invoice is not yet archived

--- silver/models.py
+++ silver/models.py
@@ -282,6 +282,21 @@ class Customer(BillingEntity):
         company_field = self._meta.get_field_by_name("company")[0]
         company_field.help_text = "The company to which the bill is issued."
 
+    def _update_historical_entries(self):
+        self_fields = set([field for field in self._meta.get_all_field_names()])
+        customer_history_fields = set(CustomerHistory._meta.get_all_field_names()) - set(['id'])
+        common_fields = self_fields.intersection(customer_history_fields)
+
+        customer_fields_values = {}
+        for field in common_fields:
+            customer_fields_values[field] = getattr(self, field)
+        self.archive_entries.filter(archived=False).update(
+            **customer_fields_values)
+
+    def save(self, *args, **kwargs):
+        super(Customer, self).save(*args, **kwargs)
+        self._update_historical_entries()
+
     def __unicode__(self):
         return " - ".join(filter(None, [self.name, self.company]))
 
@@ -297,6 +312,22 @@ class Provider(BillingEntity):
         company_field = self._meta.get_field_by_name("company")[0]
         company_field.help_text = "The provider issuing the invoice."
 
+    def _update_historical_entries(self):
+        self_fields = set([field for field in self._meta.get_all_field_names()])
+        provider_history_fields = set(ProviderHistory._meta.get_all_field_names()) - set(['id'])
+        common_fields = self_fields.intersection(provider_history_fields)
+
+        provider_fields_values = {}
+        for field in common_fields:
+            provider_fields_values[field] = getattr(self, field)
+        self.archive_entries.filter(archived=False).update(
+            **provider_fields_values)
+
+    def save(self, *args, **kwargs):
+        super(Provider, self).save(*args, **kwargs)
+        self._update_historical_entries()
+
+
     def __unicode__(self):
         return " - ".join(filter(None, [self.name, self.company]))
 
@@ -306,7 +337,7 @@ class ProductCode(models.Model):
 
 
 class CustomerHistory(BillingEntity):
-    customer_ref = models.ForeignKey('Customer', related_name='archived_entries')
+    customer_ref = models.ForeignKey('Customer', related_name='archive_entries')
     archived = models.BooleanField(default=False)
 
     def __unicode__(self):
@@ -314,7 +345,7 @@ class CustomerHistory(BillingEntity):
 
 
 class ProviderHistory(BillingEntity):
-    provider_ref = models.ForeignKey('Provider', related_name='archived_entries')
+    provider_ref = models.ForeignKey('Provider', related_name='archive_entries')
     archived = models.BooleanField(default=False)
 
     def __unicode__(self):
