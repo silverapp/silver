1583b9e325ae80daaa924aff58dce0a94e597b70
Emanuel Danci <danci.emanuel@gmail.com>
2014-12-03 11:19:25 +0200

    PEP8ify the fixed test

--- silver/tests/spec/test_customer.py
+++ silver/tests/spec/test_customer.py
@@ -54,7 +54,7 @@ class TestCustomerEndpoint(APITestCase):
 
             self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
             self.assertEqual(response.data,
-                                {field: [u'This field is required.']})
+                            {field: [u'This field is required.']})
 
     def test_get_customer_detail(self):
         customer = CustomerFactory.create()
