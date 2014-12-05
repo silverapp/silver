8df0305b2308e6e8e0b424a38e92e8f79c8d1088
Emanuel Danci <danci.emanuel@gmail.com>
2014-12-05 14:15:44 +0200

    Finish initial unit test for the invoice creation

--- silver/tests/unit/test_invoice.py
+++ silver/tests/unit/test_invoice.py
@@ -1,21 +1,33 @@
 from django.test import TestCase
 
-"""
 from silver.models import (Invoice, Customer, CustomerHistory, Provider,
                            ProviderHistory)
 from silver.tests.factories import CustomerFactory, ProviderFactory
-"""
 
 
 class TestInvoice(TestCase):
-    def test_basic_invoice_creation(self):
-        assert True
-        """
-         customer = CustomerFactory.create()
-         provider = ProviderFactory.create()
 
-         invoice = Invoice(currency='USD')
-         invoice.save(invoice_customer_id=customer.id,
-                      invoice_provider_id=provider.id)
-        """
-        # Assertions
+    def _assert_common_fields_equality(self, obj1, obj2):
+        for field in set(obj1._meta.get_all_field_names()) - set(['id']):
+            obj1_field_value = getattr(obj1, field)
+            try:
+                obj2_field_value = getattr(obj2, field)
+                assert obj1_field_value == obj2_field_value
+            except AttributeError:
+                pass
+
+    def test_basic_invoice_creation(self):
+        customer = CustomerFactory.create()
+        provider = ProviderFactory.create()
+
+        invoice = Invoice(currency='USD')
+        invoice.save(invoice_customer_id=customer.id,
+                     invoice_provider_id=provider.id)
+
+        customer_hist = CustomerHistory.objects.get(id=1)
+        assert customer_hist.customer_ref == customer
+        self._assert_common_fields_equality(customer_hist, customer)
+
+        provider_hist = ProviderHistory.objects.get(id=1)
+        assert provider_hist.provider_ref == provider
+        self._assert_common_fields_equality(provider_hist, provider)
