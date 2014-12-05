ba31b0ee16c454d800a30eb3736a3d18c4e3ec9e
Emanuel Danci <danci.emanuel@gmail.com>
2014-12-05 17:40:59 +0200

    Test the changes for a Customer/Provider.

--- silver/tests/unit/test_invoice.py
+++ silver/tests/unit/test_invoice.py
@@ -31,3 +31,36 @@ class TestInvoice(TestCase):
         provider_hist = ProviderHistory.objects.get(id=1)
         assert provider_hist.provider_ref == provider
         self._assert_common_fields_equality(provider_hist, provider)
+
+    def test_customer_hist_and_provider_hist_proper_update(self):
+        customer = CustomerFactory.create()
+        provider = ProviderFactory.create()
+
+        invoice = Invoice(currency='USD')
+        invoice.save(invoice_customer_id=customer.id,
+                     invoice_provider_id=provider.id)
+
+        customer.name = 'NewName'
+        customer.address_1 = "NewAddress"
+        customer.save()
+        provider.name = 'NewName'
+        provider.address_1 = "NewAddress"
+        provider.save()
+
+        invoice = Invoice.objects.get(id=1)
+        assert invoice.customer.name == "NewName"
+        assert invoice.customer.address_1 == "NewAddress"
+        assert invoice.provider.name == "NewName"
+        assert invoice.provider.address_1 == "NewAddress"
+
+    def test_ignore_historical_customers_and_providers(self):
+        """
+        TODO: add the test after finishing fsm transitions.
+
+        If an invoice's state is issued => the curresponding CustomerHistory
+        and ProviderHistory objects should have the attribute archived=True
+        => any updates on the related Customer/Provider objects should
+        not influence the corresponding historical CustomerHistory/ProviderFactory
+        objects.
+        """
+        assert True
