b58183cb2604f5b034fffc964e2bec17d363db84
bogdanpetrea <bogdan@presslabs.com>
2014-12-09 12:43:35 +0200

    Updated some views to use the HPList* view classes.

--- silver/api/views.py
+++ silver/api/views.py
@@ -1,11 +1,10 @@
 import datetime
 from django_filters import FilterSet, CharFilter, BooleanFilter
 
-from rest_framework import generics, permissions, status, filters, mixins
+from rest_framework import generics, permissions, status, filters
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_bulk import ListBulkCreateAPIView
 from silver.api.dateutils import last_date_that_fits
 
 from silver.models import (MeteredFeatureUnitsLog, Subscription, MeteredFeature,
@@ -15,6 +14,8 @@ from silver.api.serializers import (MeteredFeatureUnitsLogSerializer,
                                     SubscriptionDetailSerializer,
                                     PlanSerializer, MeteredFeatureSerializer,
                                     ProviderSerializer)
+from silver.api.generics import (HPListAPIView, HPListBulkCreateAPIView,
+                                 HPListCreateAPIView)
 from silver.utils import get_object_or_None
 
 
@@ -33,7 +34,7 @@ class PlanFilter(FilterSet):
                   'currency', 'provider', 'interval']
 
 
-class PlanList(generics.ListCreateAPIView):
+class PlanList(HPListCreateAPIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     serializer_class = PlanSerializer
     queryset = Plan.objects.all()
@@ -70,7 +71,7 @@ class PlanDetail(generics.RetrieveDestroyAPIView):
                         status=status.HTTP_200_OK)
 
 
-class PlanMeteredFeatures(generics.ListAPIView):
+class PlanMeteredFeatures(HPListAPIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     serializer_class = MeteredFeatureSerializer
     model = MeteredFeature
@@ -88,7 +89,7 @@ class MeteredFeaturesFilter(FilterSet):
         fields = ('name', )
 
 
-class MeteredFeatureList(ListBulkCreateAPIView):
+class MeteredFeatureList(HPListBulkCreateAPIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     serializer_class = MeteredFeatureSerializer
     queryset = MeteredFeature.objects.all()
@@ -116,7 +117,7 @@ class SubscriptionFilter(FilterSet):
         fields = ['plan', 'customer', 'company']
 
 
-class SubscriptionList(generics.ListCreateAPIView):
+class SubscriptionList(HPListCreateAPIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
@@ -312,7 +313,7 @@ class CustomerFilter(FilterSet):
         fields = ['email', 'name', 'company', 'active', 'country', 'sales_tax_name']
 
 
-class CustomerList(generics.ListCreateAPIView):
+class CustomerList(HPListCreateAPIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
@@ -339,7 +340,7 @@ class ProviderFilter(FilterSet):
         fields = ['email', 'company']
 
 
-class ProviderListBulkCreate(ListBulkCreateAPIView):
+class ProviderListBulkCreate(HPListBulkCreateAPIView):
     permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser,)
     serializer_class = ProviderSerializer
     queryset = Provider.objects.all()
