376c1053c7b52a03ffde4137df0af1299c363124
Emanuel Danci <danci.emanuel@gmail.com>
2015-01-27 19:45:59 +0200

    Remove unused import

--- silver/tests/spec/test_metered_feature.py
+++ silver/tests/spec/test_metered_feature.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
-from silver.models import ProductCode
 from silver.tests.factories import (AdminUserFactory, MeteredFeatureFactory,
                                     ProductCodeFactory)
 
