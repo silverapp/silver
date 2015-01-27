0adcde55347138bf08284565a4c123f2664f92b2
Emanuel Danci <danci.emanuel@gmail.com>
2015-01-27 14:33:00 +0200

    Add test_post_invoice

---
+++ silver/tests/spec/test_invoice.py
@@ -0,0 +1,47 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from silver.tests.factories import (AdminUserFactory, CustomerFactory,
+                                    ProviderFactory)
+
+
+class TestInvoiceEndpoints(APITestCase):
+    def setUp(self):
+        admin_user = AdminUserFactory.create()
+        self.client.force_authenticate(user=admin_user)
+
+    def test_post_invoice(self):
+        CustomerFactory.create()
+        ProviderFactory.create()
+        url = reverse('invoice-list')
+
+        data = {
+            'provider': 'http://testserver/providers/1/',
+            'customer': 'http://testserver/customers/1/',
+            'number': "",
+            'currency': 'RON',
+            'invoice_entries': []
+        }
+
+        response = self.client.post(url, data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data == {
+            "id": 1,
+            "series": "InvoiceSeries",
+            "number": 1,
+            "provider": "http://testserver/providers/1/",
+            "customer": "http://testserver/customers/1/",
+            "archived_provider": {},
+            "archived_customer": {},
+            "due_date": None,
+            "issue_date": None,
+            "paid_date": None,
+            "cancel_date": None,
+            "sales_tax_name": "VAT",
+            "sales_tax_percent": '1.00',
+            "currency": "RON",
+            "state": "draft",
+            "proforma": None,
+            "invoice_entries": []
+        }
