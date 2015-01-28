0d9b856af51afbf31f4b305054028dcd923fea45
Emanuel Danci <danci.emanuel@gmail.com>
2015-01-28 17:37:27 +0200

    Change

--- silver/management/commands/generate_invoices.py
+++ silver/management/commands/generate_invoices.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 
+from silver.models import Invoice, Plan, Subscription
+
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        self.stdout.write('Text')
+        self.stdout.write('Asd')
 
