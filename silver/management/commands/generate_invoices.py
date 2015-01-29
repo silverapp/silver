e72135fb0a87638ded869b69ee2dc0710e6d1f79
Emanuel Danci <danci.emanuel@gmail.com>
2015-01-29 13:42:59 +0200

    Generate either Invoice or Proforma based on the provider's flow

--- silver/management/commands/generate_invoices.py
+++ silver/management/commands/generate_invoices.py
@@ -2,74 +2,102 @@ from decimal import Decimal
 from django.core.management.base import BaseCommand
 
 from silver.models import (Customer, MeteredFeatureUnitsLog, Invoice,
-                           DocumentEntry)
+                           DocumentEntry, Proforma)
 
 class Command(BaseCommand):
+
+    def _add_plan_document_entry(self, subscription, plan, invoice=None,
+                                 proforma=None):
+
+        description = subscription.description or '%s plan subscription.' % plan.name
+        DocumentEntry.objects.create(invoice=invoice,
+                                     proforma=proforma,
+                                     description=description,
+                                     unit_price=plan.amount,
+                                     quantity=Decimal('1.00'),
+                                     product_code=plan.product_code)
+
+    def _add_mf_document_entries(self, subscription, invoice=None,
+                                 proforma=None):
+
+        for mf in subscription.plan.metered_features.all():
+            criteria = {'metered_feature': mf,
+                        'subscription': subscription}
+            total_units_consumed = 0
+            consumed_mf_log = MeteredFeatureUnitsLog.objects.filter(**criteria)
+            for log_item in consumed_mf_log:
+                total_units_consumed += log_item.consumed_units
+            final_units_count = max(0, total_units_consumed - mf.included_units)
+            if final_units_count != 0:
+                # Add the metered feature to the invoice
+                DocumentEntry.objects.create(invoice=invoice,
+                                             description=mf.name,
+                                             unit_price=mf.price_per_unit,
+                                             quantity=final_units_count,
+                                             product_code=mf.product_code)
+
+    def _add_plan_entry(self, provider_flow, subscription, plan, document):
+        if provider_flow == 'proforma':
+            entry_args = {'proforma': document}
+        else:
+            entry_args = {'invoice': document}
+
+        doc_entry_args = entry_args.copy()
+        doc_entry_args.update({'subscription': subscription,
+                               'plan': plan})
+        self._add_plan_document_entry(**doc_entry_args)
+
+    def _add_mf_entries(self, provider_flow, document, subscription):
+        if provider_flow == 'proforma':
+            entry_args = {'proforma': document}
+        else:
+            entry_args = {'invoice': document}
+
+        mf_entry_args = entry_args.copy()
+        mf_entry_args.update({'subscription': subscription})
+        self._add_mf_document_entries(**mf_entry_args)
 
     def handle(self, *args, **kwargs):
         for customer in Customer.objects.all():
             if customer.consolidated_billing:
                 # For now we take into consideration only the invoices
                 # we will deal with the proformas later.
-                invoice_per_provider = {}
+                document_per_provider = {}
                 for subscription in customer.subscriptions.all():
-                    plan = subscription.plan
-                    if plan.provider in invoice_per_provider:
-                        invoice = invoice_per_provider[plan.provider]
+                    provider_flow = subscription.plan.provider_flow
+                    if provider_flow == 'proforma':
+                        DocumentModel = Proforma
                     else:
-                        invoice = Invoice.objects.create(provider=plan.provider,
-                                                         customer=customer)
-                        invoice_per_provider[plan.provider] = invoice
+                        DocumentModel = Invoice
 
-                    # TODO: Add description field to Subscription and here
-                    # use either the subscription.description or plan.name
-                    DocumentEntry.objects.create(invoice=invoice,
-                                                 description=plan.name,  # TODO
-                                                 unit_price=plan.amount,
-                                                 quantity=Decimal(1),
-                                                 product_code=plan.product_code)
-                    for mf in subscription.plan.metered_features.all():
-                        criteria = {'metered_feature': mf,
-                                    'subscription': subscription}
-                        total_units_consumed = 0
-                        consumed_mf_log = MeteredFeatureUnitsLog.objects.filter(**criteria)
-                        for log_item in consumed_mf_log:
-                            total_units_consumed += log_item.consumed_units
-                        final_units_count = max(0, total_units_consumed - mf.included_units)
-                        if final_units_count != 0:
-                            # Add the metered feature to the invoice
-                            DocumentEntry.objects.create(invoice=invoice,
-                                                         description=mf.name,
-                                                         unit_price=mf.price_per_unit,
-                                                         quantity=final_units_count,
-                                                         product_code=mf.product_code)
+                    plan = subscription.plan
+                    if plan.provider in document_per_provider:
+                        document = document_per_provider[plan.provider]
+                    else:
+                        document = DocumentModel.objects.create(
+                            provider=plan.provider, customer=customer)
+                        document_per_provider[plan.provider] = document
+
+                    # Add plan to invoice/proforma
+                    self._add_plan_entry(provider_flow, subscription, plan,
+                                         document)
+                    # Add mf units to proforma/invoice
+                    self._add_mf_entries(provider_flow, document, subscription)
             else:
                 # Generate an invoice for each subscription
                 for subscription in customer.subscriptions.all():
+                    provider_flow = subscription.plan.provider_flow
+                    if provider_flow == 'proforma':
+                        DocumentModel = Proforma
+                    else:
+                        DocumentModel = Invoice
+
                     plan = subscription.plan
-                    invoice = Invoice.objects.create(provider=plan.provider,
-                                                    customer=customer)
+                    document = DocumentModel.objects.create(provider=plan.provider,
+                                                            customer=customer)
 
-                    # TODO: Add description field to Subscription and here
-                    # use either the subscription.description or plan.name
-                    DocumentEntry.objects.create(invoice=invoice,
-                                                 description=plan.name,  # TODO
-                                                 unit_price=plan.amount,
-                                                 quantity=Decimal(1),
-                                                 product_code=plan.product_code)
-                    for mf in subscription.plan.metered_features.all():
-                        criteria = {'metered_feature': mf,
-                                    'subscription': subscription}
-                        total_units_consumed = 0
-                        consumed_mf_log = MeteredFeatureUnitsLog.objects.filter(**criteria)
-                        for log_item in consumed_mf_log:
-                            total_units_consumed += log_item.consumed_units
-                        final_units_count = max(0, total_units_consumed - mf.included_units)
-                        if final_units_count != 0:
-                            # Add the metered feature to the invoice
-                            DocumentEntry.objects.create(invoice=invoice,
-                                                         description=mf.name,
-                                                         unit_price=mf.price_per_unit,
-                                                         quantity=final_units_count,
-                                                         product_code=mf.product_code)
-
+                    # Add plan to invoice/proforma
+                    self._add_plan_entry(provider_flow, subscription, plan,
+                                         document)
+                    # Add mf units to proforma/invoice
+                    self._add_mf_entries(provider_flow, document, subscription)
