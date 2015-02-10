7d2b6dc3a7235581501750befadc8f05a44fa2ff
Calin Don <calin.don@gmail.com>
2015-02-10 12:33:18 +0200

    HOOK_EVENTS are defined in __init__.py
    settings.py should import HOOK_EVENTS instead of defining them since
    their definition is silver specific.

--- settings_dev.py
+++ settings_dev.py
@@ -72,35 +72,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger error'
 }
 
-HOOK_EVENTS = {
-    # 'any.event.name': 'App.Model.Action' (created/updated/deleted)
-    'customer.created': 'silver.Customer.created+',
-    'customer.updated': 'silver.Customer.updated+',
-    'customer.deleted': 'silver.Customer.deleted+',
-
-    'plan.created': 'silver.Plan.created+',
-    'plan.updated': 'silver.Plan.updated+',
-    'plan.deleted': 'silver.Plan.deleted+',
-
-    'subscription.created': 'silver.Subscription.created+',
-    'subscription.updated': 'silver.Subscription.updated+',
-    'subscription.deleted': 'silver.Subscription.deleted+',
-
-    'metered-feature.created': 'silver.MeteredFeature.created+',
-    # changing metered features is not enabled through the API, but this can
-    # still be done through the admin panel
-    'metered-feature.updated': 'silver.MeteredFeature.updated+',
-    'metered-feature.deleted': 'silver.MeteredFeature.deleted+',
-
-    'mf-units-log.created': 'silver.MeteredFeatureUnitsLog.created+',
-    'mf-units-log.updated': 'silver.MeteredFeatureUnitsLog.updated+',
-    # removing logs is not enabled through the API, but this can still be done
-    # through the admin panel
-    'mf-units-log.deleted': 'silver.MeteredFeatureUnitsLog.deleted+',
-
-    'provider.created': 'silver.Provider.created+',
-    'provider.updated': 'silver.Provider.updated+',
-    'provider.deleted': 'silver.Provider.deleted+',
-}
+from silver import HOOK_EVENTS as _HOOK_EVENTS
+HOOK_EVENTS = _HOOK_EVENTS
 
 # DEFAULT_TARGET_URL = 'http://presslabs.com/api'
--- silver/__init__.py
+++ silver/__init__.py
@@ -15,18 +15,6 @@ HOOK_EVENTS = {
     'subscription.updated': 'silver.Subscription.updated+',
     'subscription.deleted': 'silver.Subscription.deleted+',
 
-    'metered-feature.created': 'silver.MeteredFeature.created+',
-    # changing metered features is not enabled through the API, but this can
-    # still be done through the admin panel
-    'metered-feature.updated': 'silver.MeteredFeature.updated+',
-    'metered-feature.deleted': 'silver.MeteredFeature.deleted+',
-
-    'mf-units-log.created': 'silver.MeteredFeatureUnitsLog.created+',
-    'mf-units-log.updated': 'silver.MeteredFeatureUnitsLog.updated+',
-    # removing logs is not enabled through the API, but this can still be done
-    # through the admin panel
-    'mf-units-log.deleted': 'silver.MeteredFeatureUnitsLog.deleted+',
-
     'provider.created': 'silver.Provider.created+',
     'provider.updated': 'silver.Provider.updated+',
     'provider.deleted': 'silver.Provider.deleted+',
