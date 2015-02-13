16f3405abe8074cf60a48a02db21784fa7f6d125
Bogdan Petrea <bogdan@presslabs.com>
2015-02-13 11:18:56 +0200

    Added MF Product Code validation for Plans in Admin.

--- silver/admin.py
+++ silver/admin.py
@@ -37,11 +37,30 @@ class LiveModelAdmin(admin.ModelAdmin):
     actions = ['delete_selected']
 
 
+class PlanForm(forms.ModelForm):
+    class Meta:
+        model = Plan
+
+    def clean(self):
+        metered_features = self.cleaned_data.get('metered_features')
+        print metered_features
+        product_codes = dict()
+        for mf in metered_features:
+            if product_codes.get(mf.product_code.value, None):
+                err_msg = 'A plan cannot have two or more metered features ' \
+                          'with the same product code. (%s, %s)' \
+                          % (mf.name, product_codes.get(mf.product_code.value))
+                raise ValidationError(err_msg)
+            product_codes[mf.product_code.value] = mf.name
+        return self.cleaned_data
+
+
 class PlanAdmin(admin.ModelAdmin):
     list_display = ['name', 'interval', 'interval_count', 'amount', 'currency',
                     'trial_period_days', 'due_days', 'generate_after',
                     'enabled', 'private']
     search_fields = ['due_days', 'name']
+    form = PlanForm
 
 
 class MeteredFeatureUnitsLogInLine(admin.TabularInline):
