4265a8c86fc5f34048344bb131e943025846e2f6
Bogdan Petrea <bogdan@presslabs.com>
2015-02-19 14:31:32 +0200

    Subscriptions won't use existing start_dates from the future when activated.

--- silver/models.py
+++ silver/models.py
@@ -255,9 +255,12 @@ class Subscription(models.Model):
     @transition(field=state, source=['inactive', 'canceled'], target='active')
     def activate(self, start_date=None, trial_end_date=None):
         if start_date:
-            self.start_date = start_date
-        elif self.start_date is None:
-            self.start_date = timezone.now().date()
+            self.start_date = min(timezone.now().date(), start_date)
+        else:
+            if self.start_date:
+                self.start_date = min(timezone.now().date(), self.start_date)
+            elif self.start_date is None:
+                self.start_date = timezone.now().date()
 
         if trial_end_date:
             self.trial_end = trial_end_date
