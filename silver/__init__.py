585f6172fda65e061ff23b71abab7dc53aec9f2b
Emanuel Danci <danci.emanuel@gmail.com>
2015-02-26 13:06:58 +0200

    Add invoice/proforma hooks to __init__.py

--- silver/__init__.py
+++ silver/__init__.py
@@ -18,6 +18,14 @@ HOOK_EVENTS = {
     'provider.created': 'silver.Provider.created+',
     'provider.updated': 'silver.Provider.updated+',
     'provider.deleted': 'silver.Provider.deleted+',
+
+    'invoice.created': 'silver.Invoice.created+',
+    'invoice.updated': 'silver.Invoice.updated+',
+    'invoice.deleted': 'silver.Invoice.deleted+',
+
+    'proforma.created': 'silver.Proforma.created+',
+    'proforma.updated': 'silver.Proforma.updated+',
+    'proforma.deleted': 'silver.Proforma.deleted+',
 }
 
 REQUIRED_APPS = [
