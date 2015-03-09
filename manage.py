876a3ea4290ef1d31e5a16d58f43d73923a78463
Calin Don <calin.don@gmail.com>
2015-03-09 23:53:37 +0200

    allow for settings_local.py during development

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_dev')
+    if os.path.isfile(os.path.join(os.path.dirname(__file__),
+                                   'settings_local.py')):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_dev')
 
     from django.core.management import execute_from_command_line
 
