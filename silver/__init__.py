9c0c6c6ded364660880100478ad03a7fb412b0de
Calin Don <calin.don@gmail.com>
2015-03-10 03:28:37 +0200

    django-rest-hooks is optional (closes #115)

--- silver/__init__.py
+++ silver/__init__.py
@@ -3,36 +3,27 @@ __version__ = '0.1'
 
 HOOK_EVENTS = {
     # 'any.event.name': 'App.Model.Action' (created/updated/deleted)
-    'customer.created': 'silver.Customer.created+',
-    'customer.updated': 'silver.Customer.updated+',
-    'customer.deleted': 'silver.Customer.deleted+',
+    'customer.created': 'silver.Customer.created',
+    'customer.updated': 'silver.Customer.updated',
+    'customer.deleted': 'silver.Customer.deleted',
 
-    'plan.created': 'silver.Plan.created+',
-    'plan.updated': 'silver.Plan.updated+',
-    'plan.deleted': 'silver.Plan.deleted+',
+    'plan.created': 'silver.Plan.created',
+    'plan.updated': 'silver.Plan.updated',
+    'plan.deleted': 'silver.Plan.deleted',
 
-    'subscription.created': 'silver.Subscription.created+',
-    'subscription.updated': 'silver.Subscription.updated+',
-    'subscription.deleted': 'silver.Subscription.deleted+',
+    'subscription.created': 'silver.Subscription.created',
+    'subscription.updated': 'silver.Subscription.updated',
+    'subscription.deleted': 'silver.Subscription.deleted',
 
-    'provider.created': 'silver.Provider.created+',
-    'provider.updated': 'silver.Provider.updated+',
-    'provider.deleted': 'silver.Provider.deleted+',
+    'provider.created': 'silver.Provider.created',
+    'provider.updated': 'silver.Provider.updated',
+    'provider.deleted': 'silver.Provider.deleted',
 
-    'invoice.created': 'silver.Invoice.created+',
-    'invoice.updated': 'silver.Invoice.updated+',
-    'invoice.deleted': 'silver.Invoice.deleted+',
+    'invoice.created': 'silver.Invoice.created',
+    'invoice.updated': 'silver.Invoice.updated',
+    'invoice.deleted': 'silver.Invoice.deleted',
 
-    'proforma.created': 'silver.Proforma.created+',
-    'proforma.updated': 'silver.Proforma.updated+',
-    'proforma.deleted': 'silver.Proforma.deleted+',
+    'proforma.created': 'silver.Proforma.created',
+    'proforma.updated': 'silver.Proforma.updated',
+    'proforma.deleted': 'silver.Proforma.deleted',
 }
-
-REQUIRED_APPS = [
-    # Required apps
-    'rest_framework',
-    'rest_hooks',
-    'international',
-    'django_fsm',
-    'django_filters',
-]
