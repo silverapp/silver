77064d50f0401b416af692616a859f4139ca8f29
Bogdan Petrea <bogdan@presslabs.com>
2015-03-11 13:17:15 +0200

    Added prorated field in DocumentEntry Admin.

--- silver/admin.py
+++ silver/admin.py
@@ -155,8 +155,8 @@ class ProviderAdmin(LiveModelAdmin):
 
 class DocumentEntryInline(admin.TabularInline):
     model = DocumentEntry
-    fields = ('description', 'product_code', 'unit', 'unit_price', 'quantity',
-              'start_date', 'end_date')
+    fields = ('description', 'prorated', 'product_code', 'unit', 'unit_price',
+              'quantity', 'start_date', 'end_date')
 
 
 class BillingDocumentForm(forms.ModelForm):
