8fc22eae46c221ef9f5f05ce0133e385d328d62b
Bogdan Petrea <bogdan@presslabs.com>
2015-06-03 18:30:42 +0300

    Added a test.

--- silver/tests/commands/test_generate_billing_documents.py
+++ silver/tests/commands/test_generate_billing_documents.py
@@ -15,7 +15,7 @@ from silver.utils import get_object_or_None
 
 
 class TestInvoiceGenerationCommand(TestCase):
-    def test_canceled_subscription_with_metered_features_to_draft(self):
+    def test_canceled_subscription_trial_with_metered_features_to_draft(self):
         now = dt.datetime(2015, 2, 7, tzinfo=timezone.get_current_timezone())
         billing_date = '2015-03-01'
 
@@ -82,6 +82,52 @@ class TestInvoiceGenerationCommand(TestCase):
             doc = get_object_or_None(DocumentEntry, id=6)
             assert doc.unit_price == metered_feature.price_per_unit
             assert doc.quantity == mf_units_log_after_trial.consumed_units
+
+    def test_canceled_subscription_with_metered_features_to_draft(self):
+        billing_date = '2015-03-01'
+
+        metered_feature = MeteredFeatureFactory(included_units=Decimal('0.00'))
+        plan = PlanFactory.create(interval='month', interval_count=1,
+                                  generate_after=120, enabled=True,
+                                  trial_period_days=7, amount=Decimal('200.00'),
+                                  metered_features=[metered_feature])
+        start_date = dt.date(2015, 1, 1)
+        trial_end = start_date + dt.timedelta(days=plan.trial_period_days)
+
+        subscription = SubscriptionFactory.create(
+            plan=plan, start_date=start_date, trial_end=trial_end)
+        subscription.activate()
+        subscription.cancel()
+        subscription.save()
+
+        mf_units_log = MeteredFeatureUnitsLogFactory(
+            subscription=subscription, metered_feature=metered_feature,
+            start_date=dt.datetime(2015, 2, 1),
+            end_date=dt.datetime(2015, 2, 24)
+        )
+
+        mocked_is_on_trial = PropertyMock(return_value=False)
+        mocked_last_billing_date = PropertyMock(
+            return_value=dt.date(2015, 2, 1)
+        )
+        mocked_is_billed_first_time = PropertyMock(return_value=False)
+        with patch.multiple('silver.models.Subscription',
+                            is_on_trial=mocked_is_on_trial,
+                            last_billing_date=mocked_last_billing_date,
+                            is_billed_first_time=mocked_is_billed_first_time):
+            call_command('generate_docs', billing_date=billing_date)
+
+            # Expect one Proforma
+            assert Proforma.objects.all().count() == 1
+            assert Invoice.objects.all().count() == 0
+
+            # Expect 1 entry:
+            # Extra Metered Features (+)
+            assert DocumentEntry.objects.all().count() == 1
+
+            doc = get_object_or_None(DocumentEntry, id=1)
+            assert doc.unit_price == metered_feature.price_per_unit
+            assert doc.quantity == mf_units_log.consumed_units
 
     def test_on_trial_without_consumed_units(self):
         assert True
