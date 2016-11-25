310c9a53486c00b05ac5421f36d184ed79c81d62
Krody Robert <krody.robi@gmail.com>
2016-11-25 11:54:01 +0200

    Added get tests and permission checks

--- silver/tests/spec/test_payment_method.py
+++ silver/tests/spec/test_payment_method.py
@@ -1,7 +1,9 @@
+from rest_framework import permissions
 from rest_framework.reverse import reverse
 
 from silver.api.serializers import PaymentMethodSerializer
 from silver.models import PaymentMethod
+from silver.api.views import PaymentMethodList, PaymentMethodDetail
 from silver.tests.factories import CustomerFactory, PaymentMethodFactory
 from silver.tests.spec.util.api_get_assert import APIGetAssert
 
@@ -10,14 +12,42 @@ class TestPaymentMethodEndpoints(APIGetAssert):
     serializer_class = PaymentMethodSerializer
 
     def setUp(self):
+        self.customer = CustomerFactory.create()
+
+        super(TestPaymentMethodEndpoints, self).setUp()
+
+    def create_payment_method(self, *args, **kwargs):
+        method = PaymentMethodFactory.create(*args, **kwargs)
+
+        # mysql does not store fractional time units but the object
+        # created will have them so we can't use it directly
+        #  to check the output
+        method.refresh_from_db()
+
+        return method
 
     def test_get_listing(self):
-        pass
+        PaymentMethodFactory.create(customer=CustomerFactory.create())
+        method = self.create_payment_method(customer=self.customer)
 
-    def test_post_listing(self):
-        pass
+        url = reverse('payment-method-list', kwargs={
+            'customer_pk': self.customer.pk
+        })
+
+        self.assert_get_data(url, [method])
 
     def test_get_detail(self):
+        PaymentMethodFactory.create(customer=CustomerFactory.create())
+        method = self.create_payment_method(customer=self.customer)
+
+        url = reverse('payment-method-detail', kwargs={
+            'customer_pk': self.customer.pk,
+            'payment_method_id': method.pk
+        })
+
+        self.assert_get_data(url, method)
+
+    def test_post_listing(self):
         pass
 
     def test_put_patch_detail(self):
@@ -42,45 +72,34 @@ class TestPaymentMethodEndpoints(APIGetAssert):
         pass
 
     def test_permissions(self):
-        # TODO check for the permission classes on the view object for the urls
-        # detail + listing
-        pass
+        self.assertEqual(PaymentMethodList.permission_classes,
+                         (permissions.IsAuthenticated,))
+        self.assertEqual(PaymentMethodDetail.permission_classes,
+                         (permissions.IsAuthenticated,))
 
     def test_filter_processor(self):
-        customer = CustomerFactory.create()
-        payment_method = PaymentMethodFactory.create(
-            customer=customer
-        )
-
-        # mysql does not store fractional time units but the object
-        # created will have them so we can't use it directly
-        #  to check the output
-        payment_method.refresh_from_db()
+        method = self.create_payment_method(customer=self.customer)
 
         url = reverse('payment-method-list', kwargs={
-            'customer_pk': customer.pk
+            'customer_pk': self.customer.pk
         })
 
         url_manual_processor = url + '?processor=manual'
         url_no_output = url + '?processor=random'
 
-        self.assert_get_data(url_manual_processor, [payment_method])
+        self.assert_get_data(url_manual_processor, [method])
         self.assert_get_data(url_no_output, [])
 
     def test_filter_state(self):
-        customer = CustomerFactory.create()
-        payment_method_enabled = PaymentMethodFactory.create(
-            customer=customer,
-            state=PaymentMethod.States.Enabled,
-        )
-        payment_method_enabled.refresh_from_db()
+        method = self.create_payment_method(customer=self.customer,
+                                            state=PaymentMethod.States.Enabled)
 
         url = reverse('payment-method-list', kwargs={
-            'customer_pk': customer.pk
+            'customer_pk': self.customer.pk
         })
 
         url_state_enabled = url + '?state=' + PaymentMethod.States.Enabled
         url_no_output = url + '?state=' + PaymentMethod.States.Disabled
 
-        self.assert_get_data(url_state_enabled, [payment_method_enabled])
+        self.assert_get_data(url_state_enabled, [method])
         self.assert_get_data(url_no_output, [])
