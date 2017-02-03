ed650fbcb29a21027e8475085eda464a8ea7367f
Calin Don <calin.don@gmail.com>
2017-02-03 15:35:20 +0200

    Show a sorted list of countries and currencies

--- silver/utils/international.py
+++ silver/utils/international.py
@@ -14,5 +14,8 @@
 
 import pycountry
 
-countries = [(country.alpha_2, country.name) for country in pycountry.countries]
-currencies = [(currency.alpha_3, currency.name) for currency in pycountry.currencies]
+countries = sorted([(country.alpha_2, country.name) for country in
+                    pycountry.countries])
+currencies = sorted([(currency.alpha_3,
+                      u'{} ({})'.format(currency.alpha_3, currency.name))
+                     for currency in pycountry.currencies])
