aeda415f2457bd9cdb3c48c344cda220a75d2d5a
Bogdan Petrea <bogdan@presslabs.com>
2017-05-19 13:28:44 +0300

    Linting.

--- silver/vendors/redis_server.py
+++ silver/vendors/redis_server.py
@@ -3,4 +3,4 @@ from redlock import Redlock
 from django.conf import settings
 
 
-lock_manager = Redlock([settings.LOCK_MANAGER_CONNECTION,])
+lock_manager = Redlock([settings.LOCK_MANAGER_CONNECTION])
