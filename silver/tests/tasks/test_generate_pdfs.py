6ab190cb5d748504f41d3e677fa88fa400cc85b4
Bogdan Petrea <bogdan@presslabs.com>
2017-05-19 13:25:49 +0300

    Updated tests after pdf generation refactoring.

---
+++ silver/tests/tasks/test_generate_pdfs.py
@@ -0,0 +1,71 @@
+import pytest
+from mock import patch, call, MagicMock
+
+from silver.tasks import generate_pdfs, generate_pdf
+from silver.tests.factories import InvoiceFactory, ProformaFactory
+
+
+@pytest.mark.django_db
+def test_generate_pdfs_task():
+    issued_invoice = InvoiceFactory.create()
+    issued_invoice.issue()
+
+    paid_invoice = InvoiceFactory.create()
+    paid_invoice.issue()
+    paid_invoice.pay()
+
+    canceled_invoice = InvoiceFactory.create()
+    canceled_invoice.issue()
+    canceled_invoice.cancel()
+
+    issued_invoice_already_generated = InvoiceFactory.create()
+    issued_invoice_already_generated.issue()
+    issued_invoice_already_generated.pdf.dirty = False
+    issued_invoice_already_generated.pdf.save()
+
+    issued_proforma = ProformaFactory.create()
+    issued_proforma.issue()
+
+    issued_proforma_already_generated = ProformaFactory.create()
+    issued_proforma_already_generated.issue()
+    issued_proforma_already_generated.pdf.dirty = False
+    issued_proforma_already_generated.pdf.save()
+
+    documents_to_generate = [issued_invoice, canceled_invoice, paid_invoice,
+                             issued_proforma]
+
+    for document in documents_to_generate:
+        assert document.pdf.dirty
+
+    with patch('silver.tasks.generate_pdf') as generate_pdf_mock:
+        generate_pdfs()
+
+        generate_pdf_mock.assert_has_calls([call.delay(document.id, document.kind)
+                                            for document in documents_to_generate],
+                                           any_order=True)
+
+
+@pytest.mark.django_db
+def test_generate_pdf_task(settings, tmpdir, monkeypatch):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+    invoice = InvoiceFactory.create()
+    invoice.issue()
+
+    assert invoice.pdf.dirty
+
+    generate_pdf_mock = MagicMock()
+
+    monkeypatch.setattr('silver.models.documents.pdf.generate_pdf_template_object',
+                        generate_pdf_mock)
+    generate_pdf(invoice.id, invoice.kind)
+
+    # pdf needs to be refreshed as the invoice reference in the test is not the same with the one
+    # in the task
+    invoice.pdf.refresh_from_db()
+
+    assert not invoice.pdf.dirty
+
+    assert invoice.pdf.url == settings.MEDIA_URL + invoice.get_pdf_upload_path()
+
+    assert generate_pdf_mock.call_count == 1
