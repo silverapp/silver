0cde90a652ff5e3b404ad7849456ca9aa34350d5
Ioana Vasi <vasi_ioandre@yahoo.com>
2017-07-18 16:17:18 +0300

    Added a seed command

---
+++ silver/management/commands/seed.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2017 Presslabs SRL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import logging
+
+from django.core.management.base import BaseCommand
+
+from silver.models import Transaction, Invoice, Proforma
+from silver.tests.factories import (TransactionFactory, InvoiceFactory, DocumentEntryFactory,
+                                    ProformaFactory)
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Adding paid invoices.'
+
+    def handle(self, *args, **options):
+        proforma = ProformaFactory.create(proforma_entries=[DocumentEntryFactory.create()],
+                                          state=Proforma.STATES.ISSUED)
+        proforma.create_invoice()
+
+        invoice = InvoiceFactory.create(invoice_entries=[DocumentEntryFactory.create()],
+                                        state=Invoice.STATES.ISSUED,
+                                        proforma=None)
+        TransactionFactory.create(state=Transaction.States.Settled,
+                                  invoice=invoice,
+                                  payment_method__customer=invoice.customer,
+                                  proforma=None)
+        InvoiceFactory.create_batch(size=3,
+                                    invoice_entries=[DocumentEntryFactory.create()],
+                                    state=Invoice.STATES.PAID,
+                                    proforma=None)
+        InvoiceFactory.create_batch(size=3,
+                                    invoice_entries=[DocumentEntryFactory.create()],
+                                    state=Invoice.STATES.DRAFT,
+                                    proforma=None)
+
+        InvoiceFactory.create_batch(size=3,
+                                    invoice_entries=[DocumentEntryFactory.create()],
+                                    state=Invoice.STATES.CANCELED,
+                                    proforma=None)
