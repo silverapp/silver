8061040e3c87a718692105d2e899ecfa3c19b3a6
Vlad Temian <vladtemian@gmail.com>
2017-07-21 14:52:21 +0300

    Remove namespace from celery vendor config

--- silver/vendors/celery_app.py
+++ silver/vendors/celery_app.py
@@ -1,14 +1,11 @@
 from __future__ import absolute_import
 
-import os
-
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 app = Celery('silver')
-app.config_from_object(settings, namespace='CELERY')
+app.config_from_object(settings)
 
 app.autodiscover_tasks()
 
