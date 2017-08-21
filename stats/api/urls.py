46815b0bd6e25f3492a79b39825a9846e178b7b3
Ioana Vasi <vasi_ioandre@yahoo.com>
2017-08-21 23:23:02 +0300

    Modified urls to match the views

--- stats/api/urls.py
+++ stats/api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from stats.api.views import TransactionStats, SubscriptionStats
+from stats.api.views import TransactionStats, SubscriptionStats, HomeView, SubscriptionStatsChart
 
 urlpatterns = [
     url('^stats/transactions/$', TransactionStats.as_view(), name='transaction_parameters'),
-url('^stats/subscriptions/$', SubscriptionStats.as_view(), name='transaction_parameters')
+    url('^stats/subscriptions/$', SubscriptionStats.as_view(), name='transaction_parameters'),
+    url('^stats/subscriptions/chart/$', HomeView.as_view(), name='home'),
+    url(r'^api/chart/data/$', SubscriptionStatsChart.as_view()),
 ]
