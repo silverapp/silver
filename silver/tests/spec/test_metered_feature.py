55b9eb7d81db85e15e2cb746f7f05842abaddbb1
Vlad Temian <vladtemian@gmail.com>
2017-08-23 18:15:47 +0300

    Fix link header related tests

--- silver/tests/spec/test_metered_feature.py
+++ silver/tests/spec/test_metered_feature.py
@@ -120,17 +120,17 @@ class TestMeteredFeatureEndpoint(APITestCase):
 
         assert response.status_code == status.HTTP_200_OK
         assert response._headers['link'] == \
-            ('Link', '<' + full_url + '?page=2; rel="next">, ' +
-             '<' + full_url + '?page=1; rel="first">, ' +
-             '<' + full_url + '?page=2; rel="last">')
+            ('Link', '<' + full_url + '?page=2>; rel="next", ' +
+             '<' + full_url + '?page=1>; rel="first", ' +
+             '<' + full_url + '?page=2> rel="last"')
 
         response = self.client.get(url + '?page=2')
 
         assert response.status_code == status.HTTP_200_OK
         assert response._headers['link'] == \
-            ('Link', '<' + full_url + '; rel="prev">, ' +
-             '<' + full_url + '?page=1; rel="first">, ' +
-             '<' + full_url + '?page=2; rel="last">')
+            ('Link', '<' + full_url + '>; rel="prev", ' +
+             '<' + full_url + '?page=1>; rel="first", ' +
+             '<' + full_url + '?page=2> rel="last"')
 
     """
     def test_get_metered_feature_unexisting(self):
