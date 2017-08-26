e23b14f7d7810c10877c0057d51d47f0badbc570
Ioana Vasi <vasi_ioandre@yahoo.com>
2017-08-26 10:25:48 +0300

    Modified views and url's according to review

--- stats/api/urls.py
+++ stats/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from stats.api.views import TransactionStats, SubscriptionStats, HomeView
+from stats.api.views import TransactionStats, DocumentStats, SubscriptionStats, ChartsView
 
 urlpatterns = [
     url('^stats/transactions/$', TransactionStats.as_view(), name='transaction_parameters'),
-    url('^stats/subscriptions/$', SubscriptionStats.as_view(), name='transaction_parameters'),
-    url('^stats/subscriptions/chart/$', HomeView.as_view(), name='home'),
+    url('^stats/documents/$', DocumentStats.as_view(), name='document_parameters'),
+    url('^stats/subscriptions/$', SubscriptionStats.as_view(), name='subscription_parameters'),
+    url('^stats/subscriptions/chart/$', ChartsView.as_view(), name='home'),
 ]
--- stats/api/views.py
+++ stats/api/views.py
@@ -5,34 +5,60 @@ from rest_framework import generics, permissions, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from silver.api.filters import TransactionFilter, SubscriptionFilter
-from silver.models import Transaction, Subscription
+from silver.api.filters import TransactionFilter, SubscriptionFilter, DocumentFilter
+from silver.models import Transaction, Subscription, Invoice
 from silver.utils.decorators import remember_last_query_params
 from stats.api.serializers import TransactionStatsSerializer
 from stats.stats import Stats
 from datetime import datetime, timedelta
 
 
-class HomeView(View):
+class ChartsView(View):
     def get(self, request, *args, **argv):
         return render(request, 'charts.html', {})
 
 
-# class SubscriptionStatsChart(APIView):
-#     authentication_classes = []
-#     permission_classes = []
-#
-#     def get(self, request, format=None):
-#         labels = ["plan1", "plan2", "plan3", "plan4", "plan5"]
-#         default_items = [1, 6, 9, 3, 1]
-#         default_items2 = [2, 2, 3, 9]
-#         data = {
-#             "labels": labels,
-#             "default": default_items,
-#             "default2": default_items2
-#         }
-#
-#         return Response(data)
+class SubscriptionStats(generics.ListAPIView):
+    queryset = Subscription.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = SubscriptionFilter
+
+    def list(self, request, *args, **kwargs):
+        query_params = self.request.query_params
+        result_type = query_params.get('result_type')
+        modifier = query_params.get('modifier', None)
+        # start_date = query_params.get('start_date')
+        # end_date = query_params.get('end_date')
+
+        queryset = self.queryset.filter(plan__provider=1)
+        stats = Stats(queryset, result_type, modifier, [])
+
+        return Response(data=stats.get_result())
+
+
+class DocumentStats(generics.ListAPIView):
+    queryset = Invoice.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    # filter_backends = (filters.DjangoFilterBackend,)
+    # filter_class = DocumentFilter
+
+    def list(self, request, *args, **kwargs):
+        granulations_list = []
+
+        query_params = self.request.query_params
+        result_type = query_params.get('result_type')
+        modifier = query_params.get('modifier', None)
+        if query_params.get('granulations_issue_date', None) is not None:
+            granulations_list.append({'name': 'issue_date',
+                                      'value': query_params.get('granulations_issue_date')})
+        if query_params.get('granulations_currency', None) is not None:
+            granulations_list.append({'name': 'currency'})
+
+        queryset = self.queryset.exclude(issue_date__isnull=True)
+        stats = Stats(queryset, result_type, modifier, granulations_list)
+
+        return Response(data=stats.get_result())
 
 
 class TransactionStats(generics.ListAPIView):
@@ -58,23 +84,5 @@ class TransactionStats(generics.ListAPIView):
         return Response(data=stats.get_result())
 
 
-class SubscriptionStats(generics.ListAPIView):
-    queryset = Subscription.objects.all()
-    permission_classes = (permissions.IsAuthenticated,)
-    filter_backends = (filters.DjangoFilterBackend,)
-    filter_class = SubscriptionFilter
 
-    def list(self, request, *args, **kwargs):
-        query_params = self.request.query_params
-        result_type = query_params.get('result_type')
-        modifier = query_params.get('modifier', None)
-        # start_date = query_params.get('start_date')
-        # end_date = query_params.get('end_date')
-
-        queryset = self.queryset.filter(plan__provider=1)
-        stats = Stats(queryset, result_type, modifier, {})
-
-        result = stats.get_result()
-
-        return Response(data=result)
 
