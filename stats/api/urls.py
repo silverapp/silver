fd5daf4050ccafbe0d650e49981afca1379236b7
Ioana Vasi <vasi_ioandre@yahoo.com>
2017-09-11 14:29:26 +0300

    Added more urls

--- stats/api/urls.py
+++ stats/api/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
-from stats.api.views import TransactionStats, DocumentStats, SubscriptionStats, ChartsView
+from stats.api.views import TransactionStats, DocumentStats, SubscriptionStats, ChartsView, \
+    ChartsJsView, BillingStats
 
 urlpatterns = [
-    url('^stats/transactions/$', TransactionStats.as_view(), name='transaction_parameters'),
-    url('^stats/documents/$', DocumentStats.as_view(), name='document_parameters'),
-    url('^stats/subscriptions/$', SubscriptionStats.as_view(), name='subscription_parameters'),
-    url('^stats/subscriptions/chart/$', ChartsView.as_view(), name='home'),
+    url('^stats/transactions/$', TransactionStats.as_view(), name='transaction_stats'),
+    url('^stats/documents/$', DocumentStats.as_view(), name='document_stats'),
+    url('^stats/billing/$', BillingStats.as_view(), name='billing_stats'),
+    url('^stats/subscriptions/$', SubscriptionStats.as_view(), name='subscription_stats'),
+    url('^stats/subscriptions/chart/$', ChartsView.as_view(), name='chart_billboard'),
+    url('^stats/subscriptions/chart/js/$', ChartsJsView.as_view(), name='chart_js'),
 ]
