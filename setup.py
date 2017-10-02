fb4f16d5b76929658feeee23c04a985113c40c6a
Bogdan Petrea <bogdan@presslabs.com>
2017-10-02 11:40:49 +0300

    Bumped silver version to 0.3.3.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def read(fname):
 
 setup(
     name="django-silver",
-    version='0.3.2',
+    version='0.3.3',
     description=read('DESCRIPTION'),
     long_description=read('README.rst'),
     license='Apache 2.0',
