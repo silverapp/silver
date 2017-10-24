7321999d6bf2ae54e5c34ae16647819685bc2a41
vasiioandre <vasi_ioandre@yahoo.com>
2017-10-24 12:17:50 +0300

    Improved code readability

--- stats/tests/api/test_stats.py
+++ stats/tests/api/test_stats.py
@@ -26,64 +26,140 @@ def test_stats_transactions_correct_url(api_client):
 @pytest.mark.django_db
 def test_stats_billing_log_view_is_correct(api_client, create_subscription):
     url = reverse('billing_log_stats')
-    response = api_client.get(url, {'result_type': 'total',
-                                    'granulation_plan': True,
-                                    'granulation_customer': True})
+    response = api_client.get(url, {
+        'result_type': 'total',
+        'granulation_plan': True,
+        'granulation_customer': True
+    })
 
     assert response.data == [
-        {'currency': u'USD', 'values': [
-            {'total': Decimal('20.00'), 'billing_date': '01/31/2017', 'id': 2}],
-         'plan': u'Hydrogen', 'customer_name': u'Harry Potter'},
-        {'currency': u'RON', 'values': [
-            {'total': Decimal('10.00'), 'billing_date': '01/11/2017', 'id': 1}],
-         'plan': u'Oxygen', 'customer_name': u'Harry Potter'},
-        {'currency': u'USD', 'values': [
-            {'total': Decimal('30.00'), 'billing_date': '02/20/2017', 'id': 4}],
-         'plan': u'Enterprise', 'customer_name': u'Ron Weasley'},
-        {'currency': u'RON', 'values': [
-            {'total': Decimal('20.00'), 'billing_date': '01/31/2017', 'id': 3},
-            {'total': Decimal('20.00'), 'billing_date': '01/31/2017', 'id': 3}],
-         'plan': u'Oxygen', 'customer_name': u'Ron Weasley'}
+        {
+            'currency': u'USD',
+            'values': [{
+                'total': Decimal('20.00'),
+                'billing_date': '01/31/2017',
+                'id': 2
+            }],
+            'plan': u'Hydrogen',
+            'customer_name': u'Harry Potter'
+        },
+        {
+            'currency': u'RON',
+            'values': [{
+                'total': Decimal('10.00'),
+                'billing_date': '01/11/2017',
+                'id': 1
+            }],
+            'plan': u'Oxygen',
+            'customer_name': u'Harry Potter'
+        },
+        {
+            'currency': u'USD',
+            'values': [{
+                'total': Decimal('30.00'),
+                'billing_date': '02/20/2017',
+                'id': 4
+            }],
+            'plan': u'Enterprise',
+            'customer_name': u'Ron Weasley'
+        },
+        {
+            'currency': u'RON',
+            'values': [{
+                'total': Decimal('20.00'),
+                'billing_date': '01/31/2017',
+                'id': 3
+            },
+            {
+                'total': Decimal('20.00'),
+                'billing_date': '01/31/2017', 'id': 3
+            }],
+            'plan': u'Oxygen',
+            'customer_name': u'Ron Weasley'}
     ]
 
 
 @pytest.mark.django_db
 def test_stats_document_view_is_correct(api_client, create_document):
     url = reverse('document_stats')
-    response = api_client.get(url, {'result_type': 'amount',
-                                    'granulation_issue_date': 'month',
-                                    'granulation_customer': True})
+    response = api_client.get(url, {
+        'result_type': 'amount',
+        'granulation_issue_date': 'month',
+        'granulation_customer': True
+    })
 
     assert response.data == [
-        {'currency': u'RON', 'issue_date': 'Aug 2017', 'values': [
-            {'total': Decimal('101.00'), 'id': 1}],
-         'customer_name': u'Harry Potter'},
-        {'currency': u'RON', 'issue_date': 'Aug 2017', 'values': [
-            {'total': 0, 'id': 2},
-            {'total': Decimal('202.00'), 'id': 3}],
-         'customer_name': u'Ron Weasley'},
-        {'currency': u'RON', 'issue_date': 'Jul 2017', 'values': [
-            {'total': Decimal('303.00'), 'id': 4}],
-         'customer_name': u'Ron Weasley'}
+        {
+            'currency': u'RON',
+            'issue_date': 'Aug 2017',
+            'values': [{
+                'total': Decimal('101.00'),
+                'id': 1}],
+            'customer_name': u'Harry Potter'
+        },
+        {
+            'currency': u'RON',
+            'issue_date': 'Aug 2017',
+            'values': [{
+                'total': 0,
+                'id': 2
+            },
+            {
+                'total': Decimal('202.00'),
+                'id': 3
+            }],
+            'customer_name': u'Ron Weasley'
+        },
+        {
+            'currency': u'RON',
+            'issue_date': 'Jul 2017',
+            'values': [{
+                'total': Decimal('303.00'),
+                'id': 4
+            }],
+            'customer_name': u'Ron Weasley'}
     ]
 
 
 @pytest.mark.django_db
 def test_stats_transaction_view_is_correct(api_client, create_transaction):
     url = reverse('transaction_stats')
-    response = api_client.get(url, {'result_type': 'amount',
-                                    'granulation_created_at': 'month',
-                                    'granulation_customer': True})
+    response = api_client.get(url, {
+        'result_type': 'amount',
+        'granulation_created_at': 'month',
+        'granulation_customer': True
+    })
 
     assert response.data == [
-        {'currency': u'RON', 'created_at': 'Jul 2017', 'values': [
-            {'total': Decimal('20.00'), 'id': 3}],
-         'customer_name': u'Hermione Granger'},
-        {'currency': u'RON', 'created_at': 'Sep 2017', 'values': [
-            {'total': Decimal('10.00'), 'id': 1}],
-         'customer_name': u'Hermione Granger'},
-        {'currency': u'RON', 'created_at': 'Jul 2017', 'values': [
-            {'total': Decimal('15.00'), 'id': 2},
-            {'total': Decimal('20.00'), 'id': 4}],
-         'customer_name': u'Ron Weasley'}
+        {
+            'currency': u'RON',
+            'created_at': 'Jul 2017',
+            'values': [{
+                'total': Decimal('20.00'),
+                'id': 3
+            }],
+            'customer_name': u'Hermione Granger'
+        },
+        {
+            'currency': u'RON',
+            'created_at': 'Sep 2017',
+            'values': [{
+                'total': Decimal('10.00'),
+                'id': 1
+            }],
+            'customer_name': u'Hermione Granger'
+        },
+        {
+            'currency': u'RON',
+            'created_at': 'Jul 2017',
+            'values': [{
+                'total': Decimal('15.00'),
+                'id': 2
+            },
+            {
+                'total': Decimal('20.00'),
+                'id': 4
+            }],
+            'customer_name': u'Ron Weasley'
+        }
     ]
