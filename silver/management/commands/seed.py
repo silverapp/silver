f41e46a7463e0962394a316fc4a2450c24d424ef
vasiioandre <vasi_ioandre@yahoo.com>
2017-10-24 12:11:07 +0300

    Modified seed command

--- silver/management/commands/seed.py
+++ silver/management/commands/seed.py
@@ -14,7 +14,7 @@
 
 
 import logging
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, date
 
 import pytz
 from decimal import Decimal
@@ -23,7 +23,8 @@ from django.core.management.base import BaseCommand
 
 from silver.models import Transaction, Invoice, BillingLog
 from silver.tests.factories import (ProviderFactory, CustomerFactory, PlanFactory,
-                                    SubscriptionFactory, MeteredFeatureFactory, TransactionFactory)
+                                    SubscriptionFactory, MeteredFeatureFactory, TransactionFactory,
+                                    DocumentEntryFactory, InvoiceFactory)
 import random
 
 logger = logging.getLogger(__name__)
@@ -34,7 +35,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         test_date = datetime(2017, 9, 11, 10, 56, 24, 898509, pytz.UTC)
-        test_date = test_date + timedelta(-365)
+        test_date = test_date + timedelta(-565)
         plan_names = ['Oxygen', 'Helium', 'Enterprise']
         customer_names = []
         currency = ['USD', 'RON']
@@ -44,7 +45,7 @@ class Command(BaseCommand):
         for i in range(5):
             customer_names.append(CustomerFactory.create())
 
-        for i in range(3):
+        for i in range(25):
             metered_feature = MeteredFeatureFactory(
                 included_units_during_trial=Decimal('0.00'),
                 price_per_unit=Decimal('2.5'))
@@ -56,30 +57,46 @@ class Command(BaseCommand):
             subscription = SubscriptionFactory.create(
                 plan=plan,
                 customer=random.choice(customer_names),
-                start_date=test_date.date()
+                start_date=test_date.date() + timedelta(-20)
             )
 
             subscription.activate()
             subscription.save()
 
             BillingLog.objects.create(subscription=subscription, billing_date=test_date.date(),
-                                      total=plan.amount)
+                                      total=plan.amount,
+                                      metered_features_billed_up_to=test_date.date(),
+                                      plan_billed_up_to=test_date.date() + timedelta(10))
+            BillingLog.objects.create(subscription=subscription, billing_date=test_date.date() +
+                                                                              timedelta(10),
+                                      total=plan.amount,
+                                      metered_features_billed_up_to=test_date.date() + timedelta(5),
+                                      plan_billed_up_to=test_date.date() + timedelta(10))
+            BillingLog.objects.create(subscription=subscription, billing_date=test_date.date() +
+                                                                              timedelta(15),
+                                      total=plan.amount,
+                                      metered_features_billed_up_to=test_date.date() +
+                                                                    timedelta(20),
+                                      plan_billed_up_to=test_date.date() + timedelta(11))
 
-            call_command('generate_docs',
-                         '--date=%s' % test_date.date(),
-                         '--subscription=%s' % subscription.id)
+            # call_command('generate_docs',
+            #              '--date=%s' % test_date.date(),
+            #              '--subscription=%s' % subscription.id)
+
+            entry = DocumentEntryFactory()
+            InvoiceFactory.create(invoice_entries=[entry], issue_date=test_date.date())
 
             test_date = test_date + timedelta(-15)
 
         numbers = [1, 3, 5, 7, 10]
         number = [-30, -100, -80]
-        for invoice in Invoice.objects.all()[:10]:
+        for invoice in Invoice.objects.filter(issue_date__isnull=False)[:10]:
             invoice.issue_date = invoice.issue_date + timedelta(random.choice(number))
             invoice.paid_date = invoice.issue_date + timedelta(random.choice(numbers))
             invoice.state = Invoice.STATES.PAID
             invoice.save()
 
-        for invoice in Invoice.objects.all()[11:25]:
+        for invoice in Invoice.objects.filter(issue_date__isnull=False)[11:25]:
             invoice.issue_date = invoice.issue_date + timedelta(random.choice(number))
             invoice.state = Invoice.STATES.ISSUED
             invoice.save()
@@ -87,4 +104,5 @@ class Command(BaseCommand):
             TransactionFactory.create(state=Transaction.States.Settled,
                                       invoice=invoice,
                                       payment_method__customer=invoice.customer,
-                                      proforma=None)
+                                      proforma=None,
+                                      created_at=date(2017, 3, 6))
