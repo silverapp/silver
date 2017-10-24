3654371830dac8ff46a03d85de9e159f0ec64bac
vasiioandre <vasi_ioandre@yahoo.com>
2017-10-24 12:13:08 +0300

    Modified getting the url parameters

--- stats/api/views.py
+++ stats/api/views.py
@@ -9,7 +9,7 @@ from rest_framework import generics, permissions, filters
 from rest_framework.response import Response
 
 from silver.api.filters import TransactionFilter, InvoiceFilter
-from silver.models import Transaction, Invoice, BillingLog
+from silver.models import Invoice, BillingLog, Transaction
 from stats.stats import Stats
 
 
@@ -25,29 +25,34 @@ class ChartsJsView(View):
 
 class BillingLogStats(generics.ListAPIView):
     queryset = BillingLog.objects.all()
+    granulation_fields = ['granulation_plan', 'granulation_customer']
+
     permission_classes = (permissions.IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        granulations_list = []
+        granulations = {}
 
         query_params = self.request.query_params
         result_type = query_params.get('result_type')
         modifier = query_params.get('modifier')
 
-        for url_parameter, url_parameter_values in request.GET.lists():
-            if url_parameter != 'result_type' and url_parameter != 'modifier':
-                granulations_list.append({'name': url_parameter[12:],
-                                          'value': url_parameter_values[0]})
+        for index, granulation in enumerate(self.granulation_fields):
+            if query_params.get(granulation, None) is not None:
+                if index == 0:
+                    granulations['granulation_field'] = granulation[12:]
+                else:
+                    granulations['additional_granulation_field'] = granulation[12:]
 
-        stats = Stats(queryset, result_type, modifier, granulations_list)
+        stats = Stats(queryset, result_type, modifier, granulations)
 
         return Response(data=stats.get_result())
 
 
 class DocumentStats(generics.ListAPIView):
     queryset = Invoice.objects.all()
+    granulation_fields = ['granulation_issue_date', 'granulation_paid_date', 'granulation_customer']
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (filters.DjangoFilterBackend,)
     filter_class = InvoiceFilter
@@ -55,24 +60,28 @@ class DocumentStats(generics.ListAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        granulations_list = []
+        granulations = {}
 
         query_params = self.request.query_params
         result_type = query_params.get('result_type')
         modifier = query_params.get('modifier')
 
-        for url_parameter, url_parameter_values in request.GET.lists():
-            if url_parameter != 'result_type' and url_parameter != 'modifier':
-                granulations_list.append({'name': url_parameter[12:],
-                                          'value': url_parameter_values[0]})
+        for granulation in self.granulation_fields:
+            if query_params.get(granulation, None) is not None:
+                if query_params.get(granulation) == 'True':
+                    granulations['additional_granulation_field'] = granulation[12:]
+                else:
+                    granulations['granulation_field'] = granulation[12:]
+                    granulations['time_granulation_interval'] = query_params.get(granulation)
 
-        stats = Stats(queryset, result_type, modifier, granulations_list)
+        stats = Stats(queryset, result_type, modifier, granulations)
 
         return Response(data=stats.get_result())
 
 
 class TransactionStats(generics.ListAPIView):
     queryset = Transaction.objects.all()
+    granulation_fields = ['granulation_created_at', 'granulation_paid_at', 'granulation_customer']
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (filters.DjangoFilterBackend,)
     filter_class = TransactionFilter
@@ -80,17 +89,20 @@ class TransactionStats(generics.ListAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        granulations_list = []
+        granulations = {}
 
         query_params = self.request.query_params
         result_type = query_params.get('result_type')
         modifier = query_params.get('modifier')
 
-        for url_parameter, url_parameter_values in request.GET.lists():
-            if url_parameter != 'result_type' and url_parameter != 'modifier':
-                granulations_list.append({'name': url_parameter[12:],
-                                          'value': url_parameter_values[0]})
+        for granulation in self.granulation_fields:
+            if query_params.get(granulation, None) is not None:
+                if query_params.get(granulation) == 'True':
+                    granulations['additional_granulation_field'] = granulation[12:]
+                else:
+                    granulations['granulation_field'] = granulation[12:]
+                    granulations['time_granulation_interval'] = query_params.get(granulation)
 
-        stats = Stats(queryset, result_type, modifier, granulations_list)
+        stats = Stats(queryset, result_type, modifier, granulations)
 
         return Response(data=stats.get_result())
