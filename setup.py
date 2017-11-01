4386e4a1bae04698f89a9e7ec66b5c03217bc1d1
Vlad Temian <vladtemian@gmail.com>
2017-11-01 16:00:12 +0200

    Bump silver version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def read(fname):
 
 setup(
     name="django-silver",
-    version='0.3.9',
+    version='0.3.10',
     description=read('DESCRIPTION'),
     long_description=read('README.rst'),
     license='Apache 2.0',
