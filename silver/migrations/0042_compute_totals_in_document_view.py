6968b0087b7898140ecbfdbe530d78106387e087
Vlad Temian <vladtemian@gmail.com>
2017-11-08 20:27:48 +0200

    Clean migration

--- silver/migrations/0042_compute_totals_in_document_view.py
+++ silver/migrations/0042_compute_totals_in_document_view.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from decimal import Decimal
 
 from django.db import migrations, models
-
-
-def compute_totals_for_documents(apps, schema_editor):
-    db_alias = schema_editor.connection.alias
-
-    Invoice = apps.get_model('silver', 'Invoice')
-    Proforma = apps.get_model('silver', 'Proforma')
-
-    for invoice in Invoice.objects.using(db_alias).exclude(state='draft'):
-        invoice._total = sum([])
-        invoice._total_in_transaction_currency = invoice.compute_total_in_transaction_currency()
-        invoice.save()
-
-    for proforma in Proforma.objects.using(db_alias).exclude(state='draft'):
-        proforma._total = proforma.compute_total()
-        proforma._total_in_transaction_currency = proforma.compute_total_in_transaction_currency()
-        proforma.save()
 
 
 class Migration(migrations.Migration):
@@ -52,17 +34,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='Document',
             name='_total',
-            field=models.DecimalField(max_digits=19, decimal_places=2,
-                                      null=True, blank=True),
+            field=models.DecimalField(blank=True, decimal_places=2, max_digits=19, null=True),
         ),
         migrations.AddField(
             model_name='Document',
             name='_total_in_transaction_currency',
-            field=models.DecimalField(max_digits=19, decimal_places=2,
-                                      null=True, blank=True),
+            field=models.DecimalField(blank=True, decimal_places=2, max_digits=19, null=True),
         ),
-        migrations.RunPython(compute_totals_for_documents,
-                             migrations.RunPython.noop),
         migrations.RunSQL(
             sql="""
                 DROP VIEW IF EXISTS silver_document;
@@ -71,7 +49,7 @@ class Migration(migrations.Migration):
                     paid_date, cancel_date, state, provider_id, customer_id,
                     proforma_id as related_document_id, archived_customer,
                     archived_provider, sales_tax_percent, sales_tax_name, currency, pdf_id,
-                    transaction_currency, _total, _total_in_transaction_currency,
+                    transaction_currency, _total, _total_in_transaction_currency
                     FROM silver_invoice
                 UNION
                 SELECT
