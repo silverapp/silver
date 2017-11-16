00709dd65a1a59f0230b620f5b9aab74dca26f3d
Vlad Temian <vladtemian@gmail.com>
2017-11-16 20:15:02 +0200

    Add SILVER_SHOW_PDF_STORAGE_URL for backward compatibility

--- silver/tests/spec/test_documents.py
+++ silver/tests/spec/test_documents.py
@@ -84,7 +84,7 @@ class TestDocumentEndpoints(APITestCase):
             u'transaction_currency': document.transaction_currency,
             u'state': document.state,
             u'total': document.total,
-            u'pdf_url': (u'http://testserver/pdfs/%s/' % document.pdf.pk) if document.pdf else None,
+            u'pdf_url': None,
             u'transactions': transactions,
             u'total_in_transaction_currency': document.total_in_transaction_currency
         }
