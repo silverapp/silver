1610cf707580513b0b8797db8284a72c7f056660
Bogdan Petrea <bogdan@presslabs.com>
2018-11-02 15:30:04 +0200

    Removed unused document DB view from migrations because of sqlite trouble.

--- silver/migrations/0032_auto_20170201_1342.py
+++ silver/migrations/0032_auto_20170201_1342.py
@@ -57,22 +57,6 @@ class Migration(migrations.Migration):
                 'managed': False,
             },
         ),
-        migrations.RunSQL("""
-                DROP VIEW IF EXISTS silver_document;
-                CREATE VIEW silver_document AS SELECT
-                    'invoice' AS kind, id, series, number, issue_date, due_date,
-                    paid_date, cancel_date, state, provider_id, customer_id,
-                    proforma_id as related_document_id, archived_customer,
-                    archived_provider, sales_tax_percent, sales_tax_name, currency, pdf
-                    FROM silver_invoice
-                UNION
-                SELECT
-                    'proforma' AS kind, id, series, number, issue_date, due_date,
-                    paid_date, cancel_date, state, provider_id, customer_id,
-                    NULL as related_document_id, archived_customer,
-                    archived_provider, sales_tax_percent, sales_tax_name, currency, pdf
-                    FROM silver_proforma WHERE invoice_id is NULL
-        """),
         migrations.CreateModel(
             name='PaymentMethod',
             fields=[
--- silver/migrations/0042_compute_totals_in_document_view.py
+++ silver/migrations/0042_compute_totals_in_document_view.py
@@ -41,41 +41,4 @@ class Migration(migrations.Migration):
             name='_total_in_transaction_currency',
             field=models.DecimalField(blank=True, decimal_places=2, max_digits=19, null=True),
         ),
-        migrations.RunSQL(
-            sql="""
-                DROP VIEW IF EXISTS silver_document;
-                CREATE VIEW silver_document AS SELECT
-                    'invoice' AS kind, id, series, number, issue_date, due_date,
-                    paid_date, cancel_date, state, provider_id, customer_id,
-                    proforma_id as related_document_id, archived_customer,
-                    archived_provider, sales_tax_percent, sales_tax_name, currency, pdf_id,
-                    transaction_currency, _total, _total_in_transaction_currency
-                    FROM silver_invoice
-                UNION
-                SELECT
-                    'proforma' AS kind, id, series, number, issue_date, due_date,
-                    paid_date, cancel_date, state, provider_id, customer_id,
-                    NULL as related_document_id, archived_customer,
-                    archived_provider, sales_tax_percent, sales_tax_name, currency, pdf_id,
-                    transaction_currency, _total, _total_in_transaction_currency
-                    FROM silver_proforma WHERE invoice_id is NULL
-            """,
-            reverse_sql="""
-                DROP VIEW IF EXISTS silver_document;
-                CREATE VIEW silver_document AS SELECT
-                    'invoice' AS kind, id, series, number, issue_date, due_date,
-                    paid_date, cancel_date, state, provider_id, customer_id,
-                    proforma_id as related_document_id, archived_customer,
-                    archived_provider, sales_tax_percent, sales_tax_name, currency, pdf_id,
-                    transaction_currency
-                    FROM silver_invoice
-                UNION
-                SELECT
-                    'proforma' AS kind, id, series, number, issue_date, due_date,
-                    paid_date, cancel_date, state, provider_id, customer_id,
-                    NULL as related_document_id, archived_customer,
-                    archived_provider, sales_tax_percent, sales_tax_name, currency, pdf_id,
-                    transaction_currency
-                    FROM silver_proforma WHERE invoice_id is NULL
-            """),
     ]
