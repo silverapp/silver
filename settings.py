e006d1a659cffa9bc19a3a8d3c12c5c22f5531f9
Adam Charnock <adam@adamcharnock.com>
2021-01-09 12:03:21 +0000

    Fixing warning regarding missing template context processor:
        ?: (admin.W411) 'django.template.context_processors.request' must be enabled in DjangoTemplates (TEMPLATES) in order to use the admin navigation sidebar.

--- settings.py
+++ settings.py
@@ -92,7 +92,8 @@ TEMPLATES = [
                 "django.template.context_processors.media",
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
-                "django.contrib.messages.context_processors.messages"
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
             )
         }
     }
