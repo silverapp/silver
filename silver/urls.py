be259e80b23b2ac3ee1e9878f08425a59c8902a2
Adam Charnock <adam@adamcharnock.com>
2021-01-09 11:28:33 +0000

    Fixing RemovedInDjango40Warning. url -> re_path (second round of this fix)
        Warning was:
    
            RemovedInDjango40Warning: django.conf.urls.url() is deprecated in favor of django.urls.re_path().

--- silver/urls.py
+++ silver/urls.py
@@ -17,7 +17,7 @@
 
 from __future__ import absolute_import
 
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.contrib import admin
 
 from silver.views import (pay_transaction_view, complete_payment_view,
@@ -30,26 +30,26 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls',
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'', include('silver.api.urls')),
+    re_path(r'', include('silver.api.urls')),
 
-    url(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/$',
-        pay_transaction_view, name='payment'),
-    url(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/complete$',
-        complete_payment_view, name='payment-complete'),
+    re_path(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/$',
+            pay_transaction_view, name='payment'),
+    re_path(r'pay/(?P<token>[0-9a-zA-Z-_\.]+)/complete$',
+            complete_payment_view, name='payment-complete'),
 
-    url(r'^autocomplete/invoices/$',
-        InvoiceAutocomplete.as_view(), name='autocomplete-invoice'),
-    url(r'^autocomplete/proformas/$',
-        ProformaAutocomplete.as_view(), name='autocomplete-proforma'),
-    url(r'^autocomplete/payment-method/$',
-        PaymentMethodAutocomplete.as_view(), name='autocomplete-payment-method'),
-    url(r'^autocomplete/plan/$',
-        PlanAutocomplete.as_view(), name='autocomplete-plan'),
-    url(r'^autocomplete/customer/$',
-        CustomerAutocomplete.as_view(), name='autocomplete-customer'),
-    url(r'^autocomplete/provider/$',
-        ProviderAutocomplete.as_view(), name='autocomplete-provider'),
+    re_path(r'^autocomplete/invoices/$',
+            InvoiceAutocomplete.as_view(), name='autocomplete-invoice'),
+    re_path(r'^autocomplete/proformas/$',
+            ProformaAutocomplete.as_view(), name='autocomplete-proforma'),
+    re_path(r'^autocomplete/payment-method/$',
+            PaymentMethodAutocomplete.as_view(), name='autocomplete-payment-method'),
+    re_path(r'^autocomplete/plan/$',
+            PlanAutocomplete.as_view(), name='autocomplete-plan'),
+    re_path(r'^autocomplete/customer/$',
+            CustomerAutocomplete.as_view(), name='autocomplete-customer'),
+    re_path(r'^autocomplete/provider/$',
+            ProviderAutocomplete.as_view(), name='autocomplete-provider'),
 ]
